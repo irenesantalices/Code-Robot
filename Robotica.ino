ece42f87b632e1ec0fa40afa32230fef292217aa
jorge.sepulveda <100760571+jsepcar@users.noreply.github.com>
2024-04-28 20:15:46 +0200

    Add files via upload
    Parte de control de motores de la función de movimiento. Todos los motores se mueven a la vez. 
    Función de realimentación y función de control.
    Variables para asignar los pines de los motores y asignación de modos. Puede ser interesante cambiarlo a constantes.

--- Robotica.ino
+++ Robotica.ino
@@ -4,31 +4,67 @@
 bool marcha, paro;
 bool marcha_anterior, paro_anterior;
 bool griper_cerrado;
+
 //Parámetros del robot
 const double l1,l2,l3,l4;
 
+//Asignacion de Pines
+//Motores
+int pinA_M1, pinB_M1;
+int pinA_M2, pinB_M2;
+int pinA_M3, pinB_M3;
+int pinA_M4, pinB_M4;
+int pinA_M5, pinB_M5;
+
+//Encoder
+
+
 //Variables de posición y orientacion
-double q1, q2, q3, q4, q5;
+//Si de los encoder nos llegan las qs, podemos realimentar directamente las q
+double q1_d, q2_d, q3_d, q4_d, q5_d;
+double q1_r, q2_r, q3_r, q4_r, q5_r;
+double err_q1, err_q2, err_q3, err_q4, err_q5;
+
+double rango_error;
+
 typedef struct punto{
   double x, y, z;
   double alfa, beta,gamma;
 } punto;
 punto almacen, posicion, objetivo;
 
-double rango_error;
-
-enum estados{reposo, recogida, deposito};
+//Estados
+enum estado_general{reposo, recogida, deposito}; 
+enum estado_motor{parado, moviendo, emergencia};
 
 //Funciones
 bool evaluar_flanco(bool pulsador, bool* pulsador_anterior);
+
 bool abrir_gripper();
+
 void mover_robot(punto punto_intermedio);   //orientar antes de mover (activar q5)
 void ciclo_trabajo(punto objetivo);
+bool control_motor(double q_d, double q_r, double q_err, int pinA, int pinB);
+
+double realimentacion(double q_d, double q_r);
+
 //Principal
 void setup() {
+  //Motores
+  pinMode(pinA_M1, OUTPUT);
+  pinMode(pinB_M1, OUTPUT);
   
+  pinMode(pinA_M2, OUTPUT);
+  pinMode(pinB_M2, OUTPUT);
   
+  pinMode(pinA_M3, OUTPUT);
+  pinMode(pinB_M3, OUTPUT);
   
+  pinMode(pinA_M4, OUTPUT);
+  pinMode(pinB_M4, OUTPUT);
+  
+  pinMode(pinA_M5, OUTPUT);
+  pinMode(pinB_M5, OUTPUT);
 }
 
 void loop() {
@@ -75,8 +111,41 @@ void ciclo_trabajo(punto objetivo){
 }
 
 void mover_robot(punto punto_intermedio){
+  bool fin1, fin2, fin3, fin4, fin5;
 
-
-
+  //Calculamos las q necearias
   
+  //Controlamos los motores
+  do{
+    fin1 = control_motor(q1_d, q1_r, err_q1, pinA_M1, pinB_M1);
+    fin2 = control_motor(q2_d, q2_r, err_q2, pinA_M2, pinB_M2);
+    fin3 = control_motor(q3_d, q3_r, err_q3, pinA_M3, pinB_M3);
+    fin4 = control_motor(q4_d, q4_r, err_q4, pinA_M4, pinB_M4);
+    fin5 = control_motor(q5_d, q5_r, err_q5, pinA_M5, pinB_M5);
+  }while( fin1*fin2*fin3*fin4*fin5 != true);
+}
+
+double realimentacion(double q_d, double q_r){
+  return q_d - q_r;
+}
+
+bool control_motor(double q_d, double q_r, double q_err, int pinA, int pinB){
+  q_err = realimentacion(q_d, q_r);
+  if(abs(q_err) < rango_error){
+    //Apagar motor si no lo esta
+    digitalWrite(pinA, LOW);
+    digitalWrite(pinB, LOW);
+    return true; //Acabado
+  }
+  else if(q_err > 0){
+    //Motor Sentido horario
+    digitalWrite(pinA, HIGH);
+    digitalWrite(pinB, LOW);
+  }
+  else{
+    //Motor sentido antihorario
+    digitalWrite(pinA, LOW);
+    digitalWrite(pinB, HIGH);
+  }
+  return false; //Sigue funcionando
 }
