750fc68e27ed22add2c23c107166115ea46072a1
irenesantalices <100681321+irenesantalices@users.noreply.github.com>
2024-04-30 11:36:34 +0200

    Robotica2.ino
    Añadidas n, o y a

--- Robotica.ino
+++ Robotica.ino
@@ -75,32 +75,39 @@ void ciclo_trabajo(punto objetivo){
 }
 
 void mover_robot(punto punto_intermedio){
-//declaracion de angulos
+  //decalracion orientaciones
+  const punto a={0,0,-1};
+  const punto o={1,0,0};
+  const punto n={0,1,0};
+  
+  //declaracion de angulos
   double q1,q2,q3,q4,q5;
   //declaracion de variables intermediass
   double A,B,c5,s5,q234,p,c2,s2;
   
   q1=atan2(punto_intermedio.y,punto_intermedio.x);
   
-  c5=sen(q1)*o.x-cos(q1)*o.y;      //que es n y o??
-  s5=sen(q1)*n.x-cos(q1)*n.y;
+  c5=sin(q1)*o.x-cos(q1)*o.y;      //que es n y o??
+  s5=sin(q1)*n.x-cos(q1)*n.y;
   q5=atan2(s5,c5);
   if(cos(q1)==0){
-    q234=atan2(a.z,a.y/sen(q1));
-    p=punto_intermedio.y/sen(q1);
+    q234=atan2(a.z,a.y/sin(q1));
+    p=punto_intermedio.y/sin(q1);
   }
   else{
     q234=atan2(a.z,a.x/cos(q1));
     p=punto_intermedio.x/cos(q1); 
   }
   A=p-l4*cos(q234)-l5*cos(q234);
-  B=punto_intermedio.z-l1-la*sen(q234)-l5*sen(q234);
-  q3=+arcos((A^2+B^2-l2^2-l3^2)/(2*l2*l3));
+  B=punto_intermedio.z-l1-l4*sin(q234)-l5*sin(q234);
+  q3=acos((A*A+B*B-l2*l2-l3*l3)/(2*l2*l3));
 
-  c2=(A*(l2+l3*cos(q3))+B*sen(q3)*l3)/((l2+l3*cos(q3))^2+(sen(q3)*l3)^2);
-  s2=(B(l2+l3*cos(q3))-A*l3*sen(q3))/((l2+l3*cos(q3))^2+(sen(q3)*l3)^2);
-  q2=atan2(s2/c2);
+  c2=(A*(l2+l3*cos(q3))+B*sin(q3)*l3)/(pow((l2+l3*cos(q3)),2)+pow((sin(q3)*l3),2));
+  s2=(B*(l2+l3*cos(q3))-A*l3*sin(q3))/(pow(l2+l3*cos(q3),2)+pow(sin(q3)*l3,2));
+  q2=atan2(s2,c2);
 
   q4=q234-q3-q2;
-    
+  
 }
+  
+
